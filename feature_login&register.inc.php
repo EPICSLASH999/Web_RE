2dd05adf07be27fe84971cabcdc3f8ff03c0126e
Jesus Santos <jesus.santos364@gmail.com>
2023-05-06 15:20:16 -0700

    Separadas "features" con .inc

---
+++ feature_login&register.inc.php
@@ -0,0 +1,239 @@
+<?php defined('APP') or die('Direct script access denied!'); ?>
+
+<div id="divLogin" class="divLogin">
+    <div class="wrapper class_login">
+        <span class="icon-close">
+            <ion-icon name="close"></ion-icon>
+        </span>
+        <!-- LOGIN FORM -->
+        <div class="form-box login">
+            <h2>Login</h2>
+            <form onsubmit="login.submit(event)" autocomplete="off" action="#" id ="formLogin">
+                <div class="input-box">
+                    <input type="email" autocomplete="off" name="email" required>
+                    <label>Email</label>
+                    <i class="uil uil-envelope icon"></i>
+                </div>
+                <div class="input-box">
+                    <input type="password" class="password" autocomplete="new-password" name="password" required>
+                    <label>Password</label>
+                    <i class="uil uil-lock icon"></i>
+                    <i class="uil uil-eye-slash showHidePw"></i>
+                </div>
+                <div class="remember-forgot">
+                    <!--<label><input type="checkbox"> 
+                    Remember me</label>-->
+                    <a href="#">Forgot Password?</a>
+                </div>
+                <button type="submit" class="btn">Login</button>
+                <div class="login-register">
+                    <p>Don't have an account? <a href="#" class="register-link">Register</a></p>
+                </div>
+            </form>
+        </div>
+
+        <!-- REGISTER FORM -->
+        <div class="form-box register">
+            <h2>Registration</h2>
+            <form onsubmit="signup.submit(event)" method="post" autocomplete="off" id="formRegister">
+                <div class="input-box">
+                    <input type="text" autocomplete="off" name="username" required>
+                    <label>Username</label>
+                    <i class="uil uil-user"></i>
+                </div>
+                <div class="input-box">
+                    <input type="email" autocomplete="off" name="email" required>
+                    <label>Email</label>
+                    <i class="uil uil-envelope icon"></i>
+                </div>
+                <div class="input-box">
+                    <input type="password" class="password" name="password" autocomplete="new-password" required>
+                    <label>Password</label>
+                    <i class="uil uil-lock icon"></i>
+                </div>
+                <div class="input-box">
+                    <input type="password" class="password" name="retype_password" autocomplete="new-password" required>
+                    <label>Confirm Password</label>
+                    <i class="uil uil-lock icon"></i>
+                    <i class="uil uil-eye-slash showHidePw"></i>
+                </div>
+                <div class="remember-forgot">
+                    <label><input type="checkbox"> 
+                    I agree to the terms & conditions</label>
+                </div>
+                <button type="submit" class="btn">Register</button>
+                <div class="login-register">
+                    <p>Already have an account? <a href="#" class="login-link">Login</a></p>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<script>
+    /* START OF CODE FOR LOGIN&REGISTER POPUPS EVENTS */
+    const wrapper = document.querySelector('.wrapper');
+    const loginLink = document.querySelector('.login-link');
+    const registerLink = document.querySelector('.register-link');
+    const btnPopup = document.querySelector('.btnLogin-popup');
+    const iconClose = document.querySelector('.icon-close');
+
+    registerLink.addEventListener('click', ()=> {
+        wrapper.classList.add('active');
+        cleanForms();
+    });
+
+    loginLink.addEventListener('click', ()=> {
+        wrapper.classList.remove('active');
+    });
+    
+    // If there is no acttive sesion.
+    // The button exists, so its true.
+    if (btnPopup) 
+    {
+        btnPopup.addEventListener('click', ()=> {
+            wrapper.classList.add('active-popup');
+            cleanForms();
+        });
+    }
+
+    iconClose.addEventListener('click', ()=> {
+        wrapper.classList.remove('active-popup');
+    });
+
+    function cleanForms(){
+        document.getElementById("formLogin").reset();
+        document.getElementById("formRegister").reset();
+    }
+
+        /* START OF CODE FOR SHOW/HIDE PASSWORD AND EYE-ICON */
+        const container = document.querySelector('.wrapper'),
+        pwShowHide = document.querySelectorAll('.showHidePw'),
+        pwFields = document.querySelectorAll('.password');
+
+
+        pwShowHide.forEach(eyeIcon => {
+            
+            eyeIcon.addEventListener("click", ()=>{
+                
+                pwFields.forEach(pwField =>{
+                    if (pwField.type === "password"){
+                        pwField.type = "text";
+
+                        pwShowHide.forEach(icon =>{
+                            icon.classList.replace("uil-eye-slash", "uil-eye");
+                        })
+                    } else{
+                        pwField.type = "password";
+
+                        pwShowHide.forEach(icon =>{
+                            icon.classList.replace("uil-eye", "uil-eye-slash");
+                        })
+                    }
+                })
+            })
+        })
+        /* END OF CODE FOR SHOW/HIDE PASSWORD AND EYE-ICON */
+        
+    /* END OF CODE FOR LOGIN&REGISTER POPUPS EVENTS */
+
+    /* START OF CODE FOR LOGIN */
+    var login = {
+
+        show: function(){
+            document.querySelector('.wrapper').classList.add('active-popup');
+        },
+        
+        hide: function(){
+            document.querySelector(".wrapper").classList.remove('active-popup');
+        },
+
+        submit: function(e){
+            e.preventDefault();
+            let inputs = e.currentTarget.querySelectorAll("input");
+            let form = new FormData();
+
+            for (var i = inputs.length - 1; i >= 0; i--) {
+                form.append(inputs[i].name, inputs[i].value);
+            }
+
+            form.append('data_type', 'login');
+            var ajax = new XMLHttpRequest();
+
+            ajax.addEventListener('readystatechange',function(){
+
+                if(ajax.readyState == 4)
+                {
+                    if(ajax.status == 200){
+
+                        let obj = JSON.parse(ajax.responseText);
+                        alert(obj.message);
+
+                        if(obj.success)
+                            window.location.reload();
+                    }else{
+                        alert("Please check your internet connection");
+                    }
+                }
+            });
+
+            ajax.open('post','ajax.inc.php', true);
+            ajax.send(form);
+        },
+
+    };
+    /* END OF CODE FOR LOGIN */
+
+    /* START OF CODE FOR REGISTER */
+    var signup = { 
+
+        show: function(){
+            document.querySelector(".js-signup-modal").classList.remove('hide');
+            document.querySelector(".js-login-modal").classList.add('hide');
+        },
+
+        hide: function(){
+            document.querySelector(".js-signup-modal").classList.add('hide');
+        },
+
+        submit: function(e){
+
+            e.preventDefault();
+            let inputs = e.currentTarget.querySelectorAll("input");
+            let form = new FormData();
+
+            for (var i = inputs.length - 1; i >= 0; i--) {
+                form.append(inputs[i].name, inputs[i].value);
+            }
+
+            form.append('data_type', 'signup');
+            var ajax = new XMLHttpRequest();
+
+            ajax.addEventListener('readystatechange',function(){
+
+                if(ajax.readyState == 4)
+                {
+                    if(ajax.status == 200){
+
+                        //console.log(ajax.responseText);
+                        let obj = JSON.parse(ajax.responseText);
+                        alert(obj.message);
+
+                        if(obj.success)
+                            window.location.reload();
+                    }else{
+                        alert("Please check your internet connection");
+                    }
+                }
+            });
+
+            ajax.open('post','ajax.inc.php', true);
+            ajax.send(form);
+        },
+
+
+    };
+    /* START OF CODE FOR REGISTER */
+
+
+</script>
---
+++ feature_navbar.inc.php
@@ -0,0 +1,80 @@
+<?php defined('APP') or die('direct script access denied!'); ?>
+
+<!-- START OF NAVBAR -->
+<header>
+    <a href="index.php" class="logo"><i class="ri-home-heart-fill"></i><span>Logo</span></a>
+
+    <ul class="navbar">
+        <li><a href="index.php#home" class="active">Home</a></li> 
+        <li><a href="index.php#news">Last News</a></li>
+        <li><a href="index.php#faq">FAQ</a></li>
+        <li><a href="index.php#about">About Us</a></li>
+        <li><a href="#">Guide</a>
+            <ul>
+                <li><a href="guide_re4_main.php">Resident Evil 4</a></li>
+                <li><a href="#">Resident Evil 4 Remake</a></li>
+            </ul>
+        </li>
+    </ul>
+
+    <div class="main">
+        <a href="#" class="games"><i class="ri-game-line"></i>Games</a>
+        <a href="forum_main.php">Forum</a> 
+        <?php if(logged_in()):?>
+            <div >
+                <a href="profile.php" class="user_container">
+                    <img src="<?= get_image($_SESSION['USER']['image'])?>" class="user_container_image" >
+                    <span class="user_container_name">Hi, <?= $_SESSION['USER']['username']?></span>
+                </a>
+                
+            </div>
+        <?php else:?>
+        <button class="btnLogin-popup" onclick='location.href="#"'><i class="ri-user-fill"></i>Sign In</button>
+        <?php endif;?>
+        <div class="bx bx-menu" id="menu-icon"></div>
+    </div>
+
+</header>
+<!-- END OF NAVBAR -->
+
+<script>
+    /* START OF RESPONSIVE BOX */
+    let menu = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
+
+    menu.onclick = () => {
+        menu.classList.toggle('bx-x');
+        navbar.classList.toggle('open');
+    } 
+	/* END OF RESPONSIVE BOX */
+
+    
+
+	var user = {
+
+		logout: function(){
+
+			let form = new FormData();
+			form.append('data_type', 'logout');
+			var ajax = new XMLHttpRequest();
+
+			ajax.addEventListener('readystatechange',function(){
+
+				if(ajax.readyState == 4)
+				{
+					if(ajax.status == 200){
+						let obj = JSON.parse(ajax.responseText);
+						alert(obj.message);
+
+						window.location.href = "index.php";
+					}else{
+						alert("Please check your internet connection");
+					}
+				}
+			});
+
+			ajax.open('post','ajax.inc.php', true);
+			ajax.send(form);
+		}
+	};
+</script>
