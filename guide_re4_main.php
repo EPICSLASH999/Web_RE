aaea7c710c92f5e79ccf9fdad36de8ade16550d8
Jesus Santos <jesus.santos364@gmail.com>
2023-05-06 15:24:42 -0700

    Reformateo de las pags Guia RE4

---
+++ guide_re4_main.php
@@ -0,0 +1,42 @@
+<?php
+	require('config.inc.php');
+	require('functions.php');
+?>
+
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+          
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="css/sideBarStyle.css" />
+    <link rel="stylesheet" type="text/css" href="css/guide_re4_story_Style.css?v1" />
+    <link rel="stylesheet" type="text/css" href="css/scrollBar.css">
+    
+    <!-- MISC -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  </head>
+
+  <body>
+    <?php include('feature_sidebar.inc.php') ?>
+    
+    <section id="contenido" class="sectionheader">
+      <div class="titulo">
+        <h1>Story</h1>
+      </div>
+    </section>
+
+    <section id="contenido" class="sectionstory">
+      <main>
+        
+        <p>
+          Leon is really cool.
+        </p>
+        <p>
+          Ashley the beautiful.
+        </p>
+      
+      </main>
+    </section>
+  </body>
+</html>
---
+++ guide_re4_searcher.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        
+        <title>Resident Evil 4</title>
+
+        <!-- CSS -->
+        <link rel="stylesheet" type="text/css" href="css/sideBarStyle.css" />
+        <link rel="stylesheet" type="text/css" href="css/guide_liveSearchStyle.css?v1">
+        <link rel="stylesheet" type="text/css" href="css/guide_re4_searcher_Style.css">
+        <link rel="stylesheet" type="text/css" href="css/scrollBar.css">
+        <!-- Font -->
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap" rel="stylesheet"/>
+        <!-- Icons -->
+        <link href="https://cdn.jsdelivr.net/npm/remixicon@3.0.0/fonts/remixicon.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+        <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    </head>
+
+    <body>
+        <?php include('feature_sidebar.inc.php') ?>
+
+        <div class="container">
+            <h2>What you lookin'?</h2>
+            <br><br>
+            <div class="input-box">
+                <input type="text" class="form-control" id="live_search" autocomplete="off" placeholder="Search... ">
+            </div>
+            <div id="searchresult" class="searchres"></div>
+        </div>
+
+        <!-- JS SCRIPTS-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+        <!-- START CODE FOR SEARCH BOX -->
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $("#live_search").keyup(function(){
+                    var input = $(this).val();
+
+                    if(input != ""){
+                        $.ajax({
+                            url:"livesearch.php",
+                            method:"POST",
+                            data:{input:input},
+
+                            success:function(data){
+                                $('#searchresult').html(data);
+                                $('#searchresult').css("display","block");
+                            }
+                        });
+                    } else{
+                        $('#searchresult').css("display","none");
+                    }
+                });
+            });
+        </script>
+        <!-- END CODE FOR SEARCH BOX -->
+    </body>
+</html>
